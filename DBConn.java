553fb15e369274fdb8c954670a9ce5bf4dd043cc
Alex <32937840+volodinaa87@users.noreply.github.com>
2020-01-28 21:16:26 +0300

    Update DBConn.java

--- DBConn.java
+++ DBConn.java
@@ -5,12 +5,18 @@
  */
 package my02_dbconn;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.hsqldb.lib.InputStreamWrapper;
 
 /**
  *
@@ -100,7 +106,7 @@ public class DBConn {
 //
 //    }
 //параметризованный с join    
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, IOException {
 //        Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
 //        String filter = "B%";
         try (Connection conn = DriverManager.getConnection("jdbc:ucanaccess://C:\\Users\\16696866\\Work\\ReebootSchoolJava\\msAccessDB\\AWSales.accdb")) {
@@ -115,23 +121,29 @@ public class DBConn {
 
             PreparedStatement prep = conn.prepareStatement(sqlQuery);
 //            prep.setString(1, filter);
-
+            String txt = "";
+            String prevCategory = "";
             try (ResultSet rs = prep.executeQuery()) {
 //                rs.next();
 //int i = 0;
                 while (rs.next()) {
 //                    String key = "Name"+ i +": " ;
-                    System.out.println(rs.getString(1) + "\t" + 
-                            rs.getString(2) + "\t" + 
-                            rs.getString(3) + "\t" +
-                            rs.getDouble(4));
-//                    System.out.print("ProductID: " + rs.getInt("PRODUCTID") + "\t");
-//                    System.out.print("Name: " + rs.getString("NAME") + "\t");
-//                    System.out.print("Name: " + rs.getString("NAME") + "\t");
-//                    System.out.print("ProductNumber: " + rs.getString("PRODUCTNUMBER") + "\t");
-//                    System.out.print("ListPrice: " + rs.getDouble("ListPrice") + "\n");
-////                    mapValues.put(key, rs.getString("NAME"));
-//                System.out.println("PURCHASE_COST: " + rs.getBigDecimal("PURCHASE_COST"));
+//                    System.out.println(rs.getString(1) + "\t" + 
+//                            rs.getString(2) + "\t" + 
+//                            rs.getString(3) + "\t" +
+//                            rs.getDouble(4));
+                    String category = rs.getString(1);
+                    String catText = category.equals(prevCategory) ? ";" : (prevCategory = category);
+                    txt += catText 
+                            + rs.getString(2) + ";"
+                            + rs.getString(3) + ";"
+                            + rs.getDouble(4) + "\n";
+
+                }
+                System.out.println(txt);
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\16696866\\Work\\ReebootSchoolJava\\Reports\\txt.csv"))) {
+                    writer.write(txt);
+
                 }
             }
 
