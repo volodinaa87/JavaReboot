0de40e35f960ef6ff5b3cb69c373721fdd371aa5
alex <alex@alex-G3-3590>
2020-02-11 20:32:02 +0300

    correct Date forward add report cellStyle

--- my07_ReportToExcel/src/my07_reporttoexcel/My07_ReportToExcel.java
+++ my07_ReportToExcel/src/my07_reporttoexcel/My07_ReportToExcel.java
@@ -9,6 +9,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.text.DateFormat;
 import java.util.Date;
 import java.util.List;
 import my05_middle_tier.*;
@@ -26,24 +27,61 @@ public class My07_ReportToExcel {
      */
     public static void main(String[] args) throws SQLException, FileNotFoundException, IOException {
         // TODO code application logic here
-        java.sql.Date startDate = new java.sql.Date (100,12,12);
-        java.sql.Date finalDate = new java.sql.Date (110,12,12);
+//        java.sql.Date startDate = new java.sql.Date (100,12,12);
+//        java.sql.Date finalDate = new java.sql.Date (110,12,12);
+        Date startDate = new Date (100,12,12);
+        Date finalDate = new Date (110,12,12);
         RepositoryAW repository = new RepositoryAW();
         List<ProductStatistics> products = repository.getBestSoldProductsInDateRange(startDate, finalDate);
         
         
         Workbook book = new HSSFWorkbook();
         Sheet sheet = book.createSheet("ReportFromDB_MSSQL");
+        
+        DataFormat format = book.createDataFormat();
+        DataFormat numericformat = book.createDataFormat();
+        
+        CellStyle dateStyle = book.createCellStyle();
+        dateStyle.setDataFormat(format.getFormat("dd.mm.yyyy"));
+        
+        CellStyle numericStyle = book.createCellStyle();
+        numericStyle.setDataFormat(numericformat.getFormat("#######.##"));
+        
+        
+        Row reportHeader = sheet.createRow(0);
+        Cell cellReportHeader = reportHeader.createCell(0);
+        cellReportHeader.setCellValue("Start Date");
+        cellReportHeader = reportHeader.createCell(1);
+        cellReportHeader.setCellValue(startDate);
+        cellReportHeader.setCellStyle(dateStyle);
+        
+        
+        reportHeader = sheet.createRow(1);
+        cellReportHeader = reportHeader.createCell(0);
+        cellReportHeader.setCellValue("Final Date");
+        cellReportHeader = reportHeader.createCell(1);
+        cellReportHeader.setCellValue(finalDate);
+        cellReportHeader.setCellStyle(dateStyle);
+        
             int i = 1;
+            Row rowHeader = sheet.createRow(3);
+            Cell cellHeader = rowHeader.createCell(0);
+            cellHeader.setCellValue("Product");
+            cellHeader = rowHeader.createCell(1);
+            cellHeader.setCellValue("Amount");
+            
         for (var ps : products) {
-            Row row = sheet.createRow(i++);
+            Row row = sheet.createRow(i+3);
             Cell cell = row.createCell(0);
             cell.setCellValue(ps.getProductName());
             cell = row.createCell(1);
             cell.setCellValue(ps.getSalesAmount());
- book.write(new FileOutputStream("/media/alex/FileStorage/Development/Projects/CloudRebootBackUp/excelPoi/excelReport.xls"));
+            cell.setCellStyle(numericStyle);
+            i++;
         }
-        
+        sheet.autoSizeColumn(0);
+        sheet.autoSizeColumn(1);
+ book.write(new FileOutputStream("/media/alex/FileStorage/Development/Projects/CloudRebootBackUp/excelPoi/excelReport.xls"));
     }
     
 }
